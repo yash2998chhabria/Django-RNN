3f4de150971a12b16196482e0845a0949e6e1f44
Yash Chhabria <yash2998chhabria@gmail.com>
2020-12-11 15:14:19 +0530

    Update views.py

--- Deprnn/runrnn/views.py
+++ Deprnn/runrnn/views.py
@@ -14,7 +14,8 @@ def displayform(request):
         indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
         tensor = torch.LongTensor(indexed)
         tensor = tensor.unsqueeze(1)
-        prediction = torch.sigmoid(settings.NEW_MODEL(tensor))
+        tobesig = settings.NEW_MODEL(tensor)
+        prediction = torch.sigmoid(tobesig)
         predicted = prediction.item() 
         sent_tokens = sent_tokenize(textcon)
         numeric_symptoms_sent_list=[]
@@ -23,7 +24,8 @@ def displayform(request):
             indexed = [settings.OWN_TEXT.stoi[t] for t in tokenized]
             tensor = torch.LongTensor(indexed)
             tensor = tensor.unsqueeze(1)
-            prediction = torch.sigmoid(settings.OWN_DATA_MODEL(tensor))
+            tobesig = settings.OWN_DATA_MODEL(tensor)
+            prediction = torch.sigmoid(tobesig)
             numeric_symptoms_sent_list.append(prediction.item())
         print(numeric_symptoms_sent_list)
         context = { "faketext" : predicted,
@@ -40,7 +42,8 @@ def checkhome(request):
         indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
         tensor = torch.LongTensor(indexed)
         tensor = tensor.unsqueeze(1)
-        prediction = torch.nn.functional.sigmoid(settings.NEW_MODEL(tensor)).item() 
+        prediction = torch.nn.functional.sigmoid(settings.NEW_MODEL(tensor))
+        predicted = prediction.item() * 100
         sent_tokens = sent_tokenize(textcon)
         numeric_symptoms_sent_list={}
         for sentence in sent_tokens:
@@ -50,7 +53,7 @@ def checkhome(request):
             tensor = tensor.unsqueeze(1)
             prediction = torch.nn.functional.sigmoid(settings.OWN_DATA_MODEL(tensor))
             numeric_symptoms_sent_list[sentence]=prediction.item() * 100
-        context = { "faketext" : prediction,
+        context = { "faketext" : predicted,
                     "list":numeric_symptoms_sent_list.items()
                     }
         return render(request,'contact.html',context)
