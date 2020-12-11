217cde4be26a48f7956d1a6677405fa05c0a944e
Yash Chhabria <yash2998chhabria@gmail.com>
2020-12-11 12:57:33 +0530

    static updates

--- Deprnn/Deprnn/urls.py
+++ Deprnn/Deprnn/urls.py
@@ -22,7 +22,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),   
-    path('home/', include('runrnn.urls')),
+    path('', include('runrnn.urls')),
 ]
 
 urlpatterns = urlpatterns + \
--- Deprnn/runrnn/views.py
+++ Deprnn/runrnn/views.py
@@ -34,6 +34,30 @@ def displayform(request):
     return render(request,'basicform.html')
 
 def checkhome(request):
+    if request.method == 'POST':
+        textcon = request.POST.get('textdata')
+        settings.NEW_MODEL.eval()
+        tokenized = [tok.text for tok in settings.NLP.tokenizer(textcon)]
+        indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
+        tensor = torch.LongTensor(indexed)
+        tensor = tensor.unsqueeze(1)
+        prediction = torch.sigmoid(settings.NEW_MODEL(tensor))
+        predicted = prediction.item() 
+        sent_tokens = sent_tokenize(textcon)
+        numeric_symptoms_sent_list=[]
+        for sentence in sent_tokens:
+            tokenized = [tok.text for tok in settings.NLP.tokenizer(sentence)]
+            indexed = [settings.OWN_TEXT.stoi[t] for t in tokenized]
+            tensor = torch.LongTensor(indexed)
+            tensor = tensor.unsqueeze(1)
+            prediction = torch.sigmoid(settings.OWN_DATA_MODEL(tensor))
+            numeric_symptoms_sent_list.append(prediction.item())
+        print(numeric_symptoms_sent_list)
+        context = { "faketext" : predicted,
+                    "list":numeric_symptoms_sent_list
+                    }
+        return render(request,'contact.html',context)
     return render(request,'home.html')
+
 def checkresults(request):
     return render(request,'contact.html')    
