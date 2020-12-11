0021a4750e86032ae75069e0083b9bed2c4d742e
Yash Chhabria <yash2998chhabria@gmail.com>
2020-12-11 13:44:05 +0530

    Update views.py

--- Deprnn/runrnn/views.py
+++ Deprnn/runrnn/views.py
@@ -35,27 +35,27 @@ def displayform(request):
 def checkhome(request):
     if request.method == 'POST':
         textcon = request.POST.get('newtextdata')
-        settings.NEW_MODEL.eval()
-        tokenized = [tok.text for tok in settings.NLP.tokenizer(textcon)]
-        indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
-        tensor = torch.LongTensor(indexed)
-        tensor = tensor.unsqueeze(1)
-        prediction = torch.sigmoid(settings.NEW_MODEL(tensor))
-        predicted = prediction.item() 
-        sent_tokens = sent_tokenize(textcon)
-        numeric_symptoms_sent_list={}
-        for sentence in sent_tokens:
-            tokenized = [tok.text for tok in settings.NLP.tokenizer(sentence)]
-            indexed = [settings.OWN_TEXT.stoi[t] for t in tokenized]
-            tensor = torch.LongTensor(indexed)
-            tensor = tensor.unsqueeze(1)
-            prediction = torch.sigmoid(settings.OWN_DATA_MODEL(tensor))
-            numeric_symptoms_sent_list[sentence]=prediction.item()
-        print(numeric_symptoms_sent_list)
-        context = { "faketext" : predicted,
-                    "list":numeric_symptoms_sent_list.items()
-                    }
-        return render(request,'contact.html',context)
+        # settings.NEW_MODEL.eval()
+        # tokenized = [tok.text for tok in settings.NLP.tokenizer(textcon)]
+        # indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
+        # tensor = torch.LongTensor(indexed)
+        # tensor = tensor.unsqueeze(1)
+        # prediction = torch.sigmoid(settings.NEW_MODEL(tensor))
+        # predicted = prediction.item() 
+        # sent_tokens = sent_tokenize(textcon)
+        # numeric_symptoms_sent_list={}
+        # for sentence in sent_tokens:
+        #     tokenized = [tok.text for tok in settings.NLP.tokenizer(sentence)]
+        #     indexed = [settings.OWN_TEXT.stoi[t] for t in tokenized]
+        #     tensor = torch.LongTensor(indexed)
+        #     tensor = tensor.unsqueeze(1)
+        #     prediction = torch.sigmoid(settings.OWN_DATA_MODEL(tensor))
+        #     numeric_symptoms_sent_list[sentence]=prediction.item() * 100
+        # print(numeric_symptoms_sent_list)
+        # context = { "faketext" : predicted,
+        #             "list":numeric_symptoms_sent_list.items()
+        #             }
+        return render(request,'contact.html',{"faketext":textcon})
     return render(request,'home.html')
 
 def checkresults(request):
