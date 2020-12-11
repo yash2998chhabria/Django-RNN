f626de1be58552b6b1b22102d999e5efbfa56e16
Yash Chhabria <yash2998chhabria@gmail.com>
2020-12-11 13:47:48 +0530

    Update views.py

--- Deprnn/runrnn/views.py
+++ Deprnn/runrnn/views.py
@@ -37,9 +37,9 @@ def checkhome(request):
         textcon = request.POST.get('newtextdata')
         settings.NEW_MODEL.eval()
         tokenized = [tok.text for tok in settings.NLP.tokenizer(textcon)]
-        # indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
-        # tensor = torch.LongTensor(indexed)
-        # tensor = tensor.unsqueeze(1)
+        indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
+        tensor = torch.LongTensor(indexed)
+        tensor = tensor.unsqueeze(1)
         # prediction = torch.sigmoid(settings.NEW_MODEL(tensor))
         # predicted = prediction.item() 
         # sent_tokens = sent_tokenize(textcon)
