42f3c9c0c94a92b66a492c601c40921c18300172
Yash Chhabria <yash2998chhabria@gmail.com>
2020-12-11 13:49:20 +0530

    Update views.py

--- Deprnn/runrnn/views.py
+++ Deprnn/runrnn/views.py
@@ -40,8 +40,8 @@ def checkhome(request):
         indexed = [settings.NEW_TEXT.stoi[t] for t in tokenized]
         tensor = torch.LongTensor(indexed)
         tensor = tensor.unsqueeze(1)
-        # prediction = torch.sigmoid(settings.NEW_MODEL(tensor))
-        # predicted = prediction.item() 
+        prediction = torch.sigmoid(settings.NEW_MODEL(tensor))
+        predicted = prediction.item() 
         # sent_tokens = sent_tokenize(textcon)
         # numeric_symptoms_sent_list={}
         # for sentence in sent_tokens:
