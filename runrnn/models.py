eb0a8361476ca1f2b91ff83e6ea883208349554f
Yash Chhabria <yash2998chhabria@gmail.com>
2021-01-07 20:27:09 +0530

    Update models.py

--- runrnn/models.py
+++ runrnn/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class user_data(models.Model):
-    text = models.models.CharField(max_length=5000)
+    text = models.CharField(max_length=5000)
     total_result = models.IntegerField(default = 0,null=False)
     Sent_result = models.CharField(max_length=1000,null=False)
