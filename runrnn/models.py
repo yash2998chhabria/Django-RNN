d3690b291af12ff31c1699e3320975a6eb117428
Yash Chhabria <yash2998chhabria@gmail.com>
2021-01-07 20:40:42 +0530

    Update models.py

--- runrnn/models.py
+++ runrnn/models.py
@@ -4,4 +4,4 @@ from django.core.validators import int_list_validator
 class user_data(models.Model):
     text = models.CharField(max_length=5000)
     total_result = models.IntegerField(default = 0,null=False)
-    Sent_result = models.CharField(validators=int_list_validator,max_length=1000,null=False)
+    Sent_result = models.CharField(validators=[int_list_validator],max_length=1000,null=False)
